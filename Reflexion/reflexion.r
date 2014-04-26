dbddb0eb7d6a2941478049653111836f3faa9ba1
Delaby Pierre <delaby.pierre@gmail.com>
2014-04-26 21:19:34 +0200

    tests sur les attributs

--- Reflexion/reflexion.r
+++ Reflexion/reflexion.r
@@ -12,9 +12,39 @@ awele.val = awele.data[,1:12]
 
 
 ########
+#### Ajout du nombre total de billes par joueur
+#######
+
+# Création d'une nouvelle matrice
+# * même nombre de lignes que awele
+# * 2 colonnes de plus
+newdata = matrix(data=0, nr = nrow(awele.data), nc=ncol(awele.data)+2 )
+newdata = data.frame(newdata)
+#affectation des données : les 12 premières, 2 vide, les 2 dernières
+for (i in 1:12){
+  newdata[,i] = awele.data[,i]
+  colnames(newdata)[i] = colnames(awele.data)[i]
+}
+
+# décalage de 2
+for (i in 13:14){
+  newdata[,i+2] = awele.data[,i]
+  colnames(newdata)[i+2] = colnames(awele.data)[i]
+}
+
+
+# ajout des sommes
+for(i in 1:nrow(newdata)){
+  newdata[i,13] = sum(newdata[i,1:6])  
+  newdata[i,14] = sum(newdata[i,7:12])
+}
+awele.data = newdata
+
+
+########
 #Voir les données
 ########
-plot(awele.data[,1:12], col=awele.data[,14])
+plot(awele.data[,1:14], col=awele.data[,16])
 
 ########
 #Les attributs sont ils indépendants ? Utiliser le Test de Pearson
@@ -37,7 +67,7 @@ print (correlations)
 ########
 #### ACP ?
 ########
-acp = PCA(awele.val, quanti.sup=awele.data[,14])
+acp = PCA(awele.val, quanti.sup=awele.data[,16])
 plot.PCA(acp)
 cout("ca sert à rien !")
 
@@ -46,9 +76,9 @@ cout("ca sert à rien !")
 #### NB 
 #######
 # On sélectionne les instances qui correspondent aux coups joués par le vainqueur des affrontements
-selection = awele.data [awele.data [, 14] == "G", ]
+selection = awele.data [awele.data [, 16] == "G", ]
 # Et on construit un modèle de classification avec l'algorithme Naive bayes
-model = naiveBayes (selection [, 1:12], selection [, 13])
+model = naiveBayes (selection [, 1:12], selection [, 15])
 
 
 ########
@@ -58,7 +88,7 @@ vseed=0;
 vnruns=100;
 data.val = selection[,1:12]
 data.pca = PCA(data.val)
-data.name = selection[,13]
+data.name = selection[,15]
 
 resultat = c (0, 0, 0, 0)
 #bootstrap : 026 : ridicule 
@@ -80,3 +110,21 @@ names(resultat)[4] = "RL"
 #tri des resultats
 resultat = sort(resultat);
 barplot(resultat)
+
+
+########
+#### Y a t'il un rapport entre la différente de billes J1 et J2 et le coup gagné ou perdu ?
+#######
+# affichage des valeurs et de la couleur selon gagné et perdu
+plot(awele.data[,13:14], col=awele.data[,16])
+chisq.test((awele.data[,13]-awele.data[,14], awele.data[,16])$p.value
+           
+# la somme des 3 premières cases ?
+for(i in 1:nrow(awele.data)){
+  awele.data[i,13] = sum(awele.data[i,4:6])  
+  awele.data[i,14] = sum(awele.data[i,10:12])
+}
+plot(awele.data[,13], col=awele.data[,16])
+# en fonction du nombre de cases vides !!!!!!
+# en foncion du nombre des case sà 1 ou à 2 chez ton adversaire
+
