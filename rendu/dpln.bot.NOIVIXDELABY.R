1775e099c69144f408cac0de1ff999febaaf7ea0
Delaby Pierre <delaby.pierre@gmail.com>
2014-05-20 16:15:12 +0200

    bot final

---
+++ rendu/dpln.bot.NOIVIXDELABY.R
@@ -0,0 +1,10 @@
+####################################
+#   Bot pour le match d'awele
+#   Delaby Pierre & Nivoix Ludovic
+###################################
+
+# Chargement de la source du bot
+source("dpln.bot.adlsupp.R")
+
+# Choix du bot
+dpln.botawele = dpln.adlkiller
