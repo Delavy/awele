de0339c42f8c2f603823e1cc59737e96c7f640ad
Delaby Pierre <delaby.pierre@gmail.com>
2014-05-20 15:56:32 +0200

    Re-effectuation du championnat

--- rendu/matchs.R
+++ rendu/matchs.R
@@ -4,24 +4,23 @@
 # Chargement du jeu d'Awélé
 source ("awele.R")
 source ("bot.nb.R")       #nb + nb2
-source ("bot.random.R")   #random
 
 ###########################################################
 # Nouveaux bots du rendu
-source ("rendu/dpln.bot.adl.R")
-source ("rendu/dpln.bot.adlsupp.R")
-source ("rendu/dpln.bot.afd.R")
-source ("rendu/dpln.bot.cart.R")
-source ("rendu/dpln.bot.cartsupp.R")
-source ("rendu/dpln.bot.knn.R")
-source ("rendu/dpln.bot.mlp.R")
-source ("rendu/dpln.bot.mlpsupp.R")
-source ("rendu/dpln.bot.multibot.R")
-source ("rendu/dpln.bot.nbsupp.R")
-source ("rendu/dpln.bot.rl.R")
-source ("rendu/dpln.bot.svm.R")
-source ("rendu/dpln.bot.svmsupp.R")
+source ("dpln.bot.adl.R")
+source ("dpln.bot.adlsupp.R")
+source ("dpln.bot.afd.R")
+source ("dpln.bot.cart.R")
+source ("dpln.bot.cartsupp.R")
+source ("dpln.bot.knn.R")
+source ("dpln.bot.mlp.R")
+source ("dpln.bot.mlpsupp.R")
+source ("dpln.bot.multibot.R")
+source ("dpln.bot.nbsupp.R")
+source ("dpln.bot.rl.R")
+source ("dpln.bot.svm.R")
+source ("dpln.bot.svmsupp.R")
 
 
 # MATCH
-awele.match (dpln.adlkiller, dpln.adl12)
+awele.match (dpln.adlkiller, dpln.nbsupp)
